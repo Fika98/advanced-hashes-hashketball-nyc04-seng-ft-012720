5b373f04dfd71a1ab3e43ba97f09d8e04915d68e
Firuz Afzalov <afzalov98@gmail.com>
2020-01-24 07:56:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -196,26 +196,7 @@ def player_stats(player_name)
   hash
 end 
 
-def big_shoe_rebounds
-  playerrebound = nil 
-  numbs = []
-  biggest_shoesize = nil 
-      game_hash.collect do |team, stats|
-        stats[:players].collect do |player, data|
-          numbs.push(data[:shoe])
-          biggest_shoesize = numbs.sort[-1] 
-             data.collect do |category, stat|
-               if category == :shoe 
-                if stat == biggest_shoesize
-                  playerrebound =  data[:rebounds]
-                end 
-              end
-      end
-      end
-    end
-  return playerrebound
-end
-  
+
   
  
   
