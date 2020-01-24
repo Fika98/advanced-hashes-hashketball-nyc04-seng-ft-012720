8ad121efdcb6f3fc3c1d617b86c04e0d70e360ba
Firuz Afzalov <afzalov98@gmail.com>
2020-01-24 07:56:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -196,8 +196,17 @@ def player_stats(player_name)
   hash
 end 
 
-
+def big_shoe_rebounds
+  player_with_largest_shoe = find_player_with_longest_shoe()
   
+  game_hash().each do |location, team_data|
+    team_data[:players].each do |player_name, player_stats|
+      if player_name == player_with_largest_shoe
+        return player_stats[:rebounds]
+      end
+    end
+  end
+end
  
   
   
